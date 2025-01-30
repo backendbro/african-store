b0a7063a635642257e0230e398b010961f5134bf
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-01-30 12:32:52 +0600

    Updated the cors

--- src/server.js
+++ src/server.js
@@ -1,33 +1,40 @@
-const dotenv = require("dotenv") 
-dotenv.config() 
+const dotenv = require("dotenv");
+dotenv.config();
 
-const express = require("express")
-const cors = require("cors")
-const cookieParser  = require("cookie-parser")
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const connectDb  = require("./db/database")
+const connectDb = require("./db/database");
 
-const Auth = require("./routes/Auth")
-const Category = require("./routes/Category")
-
+const Auth = require("./routes/Auth");
+const Category = require("./routes/Category");
 
 const main = async () => {
-    const app = express() 
-    app.use(express.json())
-    app.use(cookieParser())
-    app.use(cors())
+  const app = express();
+  app.use(express.json());
+  app.use(cookieParser());
+  //app.use(cors())
 
-    connectDb()
-
-    app.use("/api/v1/auth", Auth)
-    app.use("/api/v1/category", Category)
-
-    const port = process.env.port 
-    const server = app.listen(port, () => {
-        console.log(`server running on localhost:${port}`)
+  app.use(
+    cors({
+      origin: "http://127.0.0.1:5501",
+      methods: "GET,POST,PUT,DELETE",
+      allowedHeaders: "Content-Type,Authorization",
     })
-}
+  );
 
-main().catch(error => {
-    console.log(error)
-})
+  connectDb();
+
+  app.use("/api/v1/auth", Auth);
+  app.use("/api/v1/category", Category);
+
+  const port = process.env.port;
+  const server = app.listen(port, () => {
+    console.log(`server running on localhost:${port}`);
+  });
+};
+
+main().catch((error) => {
+  console.log(error);
+});
