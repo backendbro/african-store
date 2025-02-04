acb8808136e75b3108a1943d098a22125d026acb
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-05 04:26:01 +0600

    Added an origin

--- src/server.js
+++ src/server.js
@@ -16,7 +16,10 @@ const main = async () => {
   const app = express();
   app.use(
     cors({
-      origin: "http://127.0.0.1:5500", // Explicitly allow your frontend origin
+      origin: [
+        "http://127.0.0.1:5500",
+        "https://african-store-client.vercel.app",
+      ], // Explicitly allow your frontend origin
       methods: "GET,POST,PUT,DELETE",
       allowedHeaders: ["Content-Type", "Authorization"], // Use an array for headers
       credentials: true, // Optional: Allow cookies/authentication headers
