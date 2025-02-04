7c1f880575c9ffdbe88ac7c50252429fd9c33ca9
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-05 00:41:49 +0600

    Added more Nigerian locations

--- src/controller/Payment.js
+++ src/controller/Payment.js
@@ -3,7 +3,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.payment = async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"], // Only specify 'card' here
+      payment_method_types: ["card", "apple_pay", "google_pay"],
       line_items: req.body.items.map((item) => ({
         price_data: {
           currency: "usd",
@@ -14,7 +14,7 @@ exports.payment = async (req, res) => {
       })),
       mode: "payment",
       billing_address_collection: "required",
-      shipping_address_collection: { allowed_countries: ["US", "BR"] },
+      shipping_address_collection: { allowed_countries: ["US", "BR", "NG"] },
       success_url: `${process.env.BASE_URL}/success.html`,
       cancel_url: `${process.env.BASE_URL}/cancel.html`,
     });
