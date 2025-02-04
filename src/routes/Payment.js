492e3fddde1b2a744a8f20a963c7be34aae32955
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-04 23:11:10 +0600

    Edited the cors origin

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { payment } = require("../controller/Payment");
 const { protect } = require("../middleware/Auth");
 
-router.post("/create-checkout-session", protect, payment);
+router.post("/create-checkout-session", payment);
 
 module.exports = router;
