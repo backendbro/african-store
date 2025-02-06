dcc251daf5d8b1ad126176a7c20ce99200189a67
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-07 04:16:25 +0600

    Added a new cancel

--- src/controller/Payment.js
+++ src/controller/Payment.js
@@ -2,25 +2,31 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.payment = async (req, res) => {
   try {
+    console.log(req.body);
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"], // Only specify 'card' here
+
       line_items: req.body.items.map((item) => ({
         price_data: {
-          currency: "usd",
-          product_data: { name: item.name },
-          unit_amount: item.price * 100,
+          currency: item.currency,
+          product_data: {
+            name: item.name,
+            images: item.images || [], // Ensure images are passed, default to an empty array if none
+          },
+          unit_amount: item.price * 100, // Convert to cents
         },
         quantity: item.quantity,
       })),
       mode: "payment",
       billing_address_collection: "required",
-      shipping_address_collection: { allowed_countries: ["US", "BR"] },
-      success_url: `${process.env.BASE_URL}/success.html`,
-      cancel_url: `${process.env.BASE_URL}/cancel.html`,
+      shipping_address_collection: { allowed_countries: [] },
+      success_url: `${process.env.BASE_URL}/public/success.html`,
+      cancel_url: `${process.env.BASE_URL}/public/canceled.html`,
     });
 
     res.json({ id: session.id });
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: err.message });
   }
 };
--- src/controller/Product.js
+++ src/controller/Product.js
@@ -24,6 +24,35 @@ cloudinary.config({
   api_secret: process.env.cloudinary_api_secret,
 });
 
+exports.getCategoryProducts = async (req, res) => {
+  try {
+    const { productId, limit = 5 } = req.query;
+
+    if (!productId) {
+      return res.status(400).json({ error: "Product ID is required" });
+    }
+
+    // Find the product by ID to get its category
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    // Fetch other products from the same category
+    const products = await Product.find({
+      category: product.category,
+      _id: { $ne: productId },
+    })
+      .limit(parseInt(limit))
+      .lean();
+
+    res.status(200).json({ data: products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 exports.createProducts = async (req, res) => {
   try {
     console.log(req.body);
