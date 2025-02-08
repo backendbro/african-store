90326bd53d199592ae60778ca50e250c0c419a66
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-08 22:56:04 +0600

    Edited the controller

--- src/controller/Category.js
+++ src/controller/Category.js
@@ -53,7 +53,7 @@ exports.getCategoriesFrontEnd = async (req, res) => {
 };
 
 exports.getCategory = async (req, res) => {
-  const category = await Category.findById(req.body.id).populate("material");
+  const category = await Category.findById(req.body.id).populate("product");
 
   if (!category) {
     return `Category not found with ID`;
