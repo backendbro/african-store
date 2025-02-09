af8a4ee01813f6860c85c1ea315ecf301d53254a
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-09 15:05:51 +0600

    Added a count

--- src/controller/Review.js
+++ src/controller/Review.js
@@ -1,21 +1,32 @@
 const Review = require("../model/Reviews");
 
 // Create a review for a specific product
+
 exports.createReview = async (req, res) => {
   try {
     const { productId, name, email, rating, comment, termsAccepted } = req.body;
 
     if (
-      !productId ||
       !name ||
       !email ||
       !rating ||
       !comment ||
-      !termsAccepted
+      !termsAccepted ||
+      !productId
     ) {
       return res.status(400).json({ message: "All fields are required." });
     }
 
+    // Count user's existing reviews for the product
+    const userReviews = await Review.countDocuments({ productId, email });
+
+    if (userReviews >= 2) {
+      return res
+        .status(400)
+        .json({ message: "You can only submit up to 2 reviews per product." });
+    }
+
+    // Create a new review if limit is not reached
     const newReview = new Review({
       productId,
       name,
@@ -46,7 +57,7 @@ exports.getReviews = async (req, res) => {
 
     const reviews = await Review.find({ productId }).sort({ createdAt: -1 });
 
-    res.status(200).json(reviews);
+    res.status(200).json({ data: reviews, count: reviews.length });
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
