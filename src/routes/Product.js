ce21a8e8cdf1379f34c928302f84e2b7843af116
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-12 03:04:50 +0600

    Done

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -19,19 +19,30 @@ const upload = multer({ storage: storage }).array("images[]", 10);
 router.post(
   "/create/:categoryId",
   protect,
-  authorize("admin", "owner"),
+  authorize("admin", "owner", "user"),
   upload,
   createProducts
 );
-router.get("/", protect, authorize("admin", "owner"), getProducts);
+router.get("/", protect, authorize("admin", "owner", "user"), getProducts);
 router.get(
   "/category",
   protect,
-  authorize("admin", "owner"),
+  authorize("admin", "owner", "user"),
   getCategoryProducts
 );
-router.get("/:id", protect, authorize("admin", "owner"), getProduct);
-router.put("/:id", protect, authorize("admin", "owner"), upload, updateProduct);
-router.delete("/:id", protect, authorize("admin", "owner"), deleteProduct);
+router.get("/:id", protect, authorize("admin", "owner", "user"), getProduct);
+router.put(
+  "/:id",
+  protect,
+  authorize("admin", "owner", "user"),
+  upload,
+  updateProduct
+);
+router.delete(
+  "/:id",
+  protect,
+  authorize("admin", "owner", "user"),
+  deleteProduct
+);
 
 module.exports = router;
--- src/routes/Review.js
+++ src/routes/Review.js
@@ -3,7 +3,12 @@ const { createReview, getReviews } = require("../controller/Review");
 const { protect, authorize } = require("../middleware/Auth");
 
 const router = express.Router();
-router.post("/", protect, authorize("owner", "admin"), createReview);
-router.get("/:productId", protect, authorize("owner", "admin"), getReviews);
+router.post("/", protect, authorize("owner", "admin", "user"), createReview);
+router.get(
+  "/:productId",
+  protect,
+  authorize("owner", "admin", "user"),
+  getReviews
+);
 
 module.exports = router;
