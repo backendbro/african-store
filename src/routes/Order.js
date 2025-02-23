e2b24002adf9e0cb79beea518c8bd5b9aa622211
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-23 17:40:32 +0600

    Done

--- src/routes/Order.js
+++ src/routes/Order.js
@@ -14,8 +14,8 @@ const { protect, authorize } = require("../middleware/Auth");
 // Define routes
 router.post("/", protect, authorize("admin"), createOrder);
 router.get("/", protect, authorize("admin"), getOrders);
-router.get("/pagination", protect, authorize("admin"), getOrdersPagination);
 router.get("/order-stats", protect, authorize("admin"), getDashboardStats);
+router.get("/pagination", protect, authorize("admin"), getOrdersPagination);
 router.get("/:id", protect, authorize("admin"), getOrderById);
 router.put("/:id", protect, authorize("admin"), updateByOrderId);
 router.put("/status/:id", protect, authorize("admin"), updateOrderStatus);
