53587c6d3ec30845a9abc399b95e379f818a864c
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-02-25 03:28:55 +0600

    Doners

--- src/routes/WishList.js
+++ src/routes/WishList.js
@@ -8,7 +8,12 @@ const {
 } = require("../controller/Wishlist");
 const { protect, authorize } = require("../middleware/Auth");
 
-router.post("/", protect, authorize("owner", "admin", "user"), addToWishlist);
+router.post(
+  "/create-wishlist",
+  protect,
+  authorize("owner", "admin", "user"),
+  addToWishlist
+);
 router.get("/", protect, authorize("owner", "admin", "user"), getWishlist);
 router.get("/user-wish", protect, authorize("user", "admin"), displayProducts);
 router.put(
