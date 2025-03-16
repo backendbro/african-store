f4b4e9356e0eb84d495b7d5829bcfbfc3d74b0a9
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-03-17 01:05:25 +0600

    Done

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers, updateUser } = require("../controller/Admin");
+const {
+  getAllUsers,
+  updateUser,
+  toggleUserRole,
+} = require("../controller/Admin");
 const { protect, authorize } = require("../middleware/Auth");
 
 // Define routes
 router.get("/", getAllUsers);
 router.put("/update-user/:id", updateUser);
+router.put("/toggle-role/:id", toggleUserRole);
 
 module.exports = router;
