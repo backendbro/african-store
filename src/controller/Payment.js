129769302f0516080c5a7c3b936c3f302c8d35c1
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-03-25 02:29:20 +0600

    Done

--- src/controller/Payment.js
+++ src/controller/Payment.js
@@ -5,26 +5,6 @@ const stripe = require("stripe")(
 exports.payment = async (req, res) => {
   try {
     console.log(req.body);
-    // const session = await stripe.checkout.sessions.create({
-    //   payment_method_types: ["card"], // Only specify 'card' here
-
-    //   line_items: req.body.items.map((item) => ({
-    //     price_data: {
-    //       currency: item.currency,
-    //       product_data: {
-    //         name: item.name,
-    //         images: item.images || [], // Ensure images are passed, default to an empty array if none
-    //       },
-    //       unit_amount: item.price * 100, // Convert to cents
-    //     },
-    //     quantity: item.quantity,
-    //   })),
-    //   mode: "payment",
-    //   billing_address_collection: "required",
-    //   shipping_address_collection: { allowed_countries: [] },
-    //   success_url: `${process.env.BASE_URL}/public/success.html`,
-    //   cancel_url: `${process.env.BASE_URL}/public/canceled.html`,
-    // });
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -75,7 +55,7 @@ exports.payment = async (req, res) => {
           },
         },
       ],
-
+      expand: ["line_items"],
       success_url: `https://www.africanmarkets.eu/public/success.html`,
       cancel_url: `https://www.africanmarkets.eu/public/canceled.html`,
     });
