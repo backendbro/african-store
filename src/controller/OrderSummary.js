137d8c499cf341eaf86a5fbf80a07321159c000e
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-03-30 03:03:43 +0600

    Done

--- src/controller/OrderSummary.js
+++ src/controller/OrderSummary.js
@@ -5,7 +5,22 @@ const path = require("path");
 
 // Set up authentication with your service account including Drive scope
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "google.json"), // Absolute path relative to the current file
+  keyFile: {
+    type: "service_account",
+    project_id: "contract-ai-453807",
+    private_key_id: "34a0824f48ff86813c59bb8d9fb9d2e4d138c010",
+    private_key:
+      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDUDVe4GFhdRAo/\n9jpnMPTk0yOhkmmoa00UUStlfckCEBWh66hfiWKyRDzWkmCSB5TIypeGebcs1Ybj\nXvZCmIZkrubV71IKtZmKNIYNzF4lYciR1jXVFgwk/gVw+Y6OmXgXpgimCCa44j+z\npo3g/OmUO1cmZ/Wg0KUU77QFcamnydb6NC1kXZFq4KZIBuf0lNx/wDZE4xdDrT/h\nmeDOf9pPFVr5pccjryTnq//mA8v9PxDUAc5HpyUY6TyyLRue6jpfFweMEtIrkVZm\nduXi1sh+W80Cab9c9+1nGbod/fpuikmzC9QGxGGDsEXsRrn7CujboxbFDM6WCaII\nAsswSH1HAgMBAAECggEAP2jsAbd5+Q3HGOBbD4+Cs8h1f7PGF6BakMhC91r5WD4S\nEfTjyQnOsysXnDxyV0igE7kNPZ+5CFWju+siLcDpc0Mf8PpbKKusSbkyGv8xafFH\nJmENgPZSVF+HpgRiPj8jO8MhE/EdDy1Myj50b8KBYJusEGaG+3tY2h1h0jzudCcq\nXEbxzMXMxW7IYx5HGfiBJk1zLrLycP+ol7fI3E91JWrJig6kyOEe4qFDEBBJid/F\n2I/OvsX2nTKPKoaqnWyWObdxKqObODJqlwSoOkEowt75qX6UM+Ritm6q3WA9t5lC\nQJH5WAfN4xxyoOYNAKOCbcd9zstWN7VEiKpIGKj4WQKBgQDtlR4YtaLhZ7Q5RLGK\nzg+cNAAaxHLQRFaveJKkL7KioXL/sdA4vBjfhlpYQhvJ/8x1tPOLfe0UPpAk63Z0\nv+m2ZxzMVoW5DXRt9yVoUiMY1iKT/CnV9gEGNsVI4SohdsjE32/VIbDHSUqeLaDK\nvWtwAm7aEPl5cJTawXQIqtgyeQKBgQDkfZGTi+b7tXSFtANv5HkgkZOpQHEZNMbx\nZbY+8A/Ms4vI0k4TmaAppqD1BvAhrbHVgGRPBTS6VCnrA96drHPYhv6M0zd5A88b\nsB0Siskv/juolqMdgq0dsIYrO6aGTSenK9HyULF5n850qGL9DwwLHCBUAP6VAPBl\nW2iPf5Q9vwKBgQC1EAIB3tulYT1ur7WHDYfECbVSsQi3RH2z+PX4/wjq/JQB3mfL\n9T4Xi/LqdGdtSO9vfVpmoq+9z/hWysRaMbBR/L9r0zzSTTjAXaw6lu58jSWJFpJj\np7ocHD26Ty+mlTppH8fA7I4nMuoVJrz2BeoNUS1eL7WRIYWwOeLOgDWIMQKBgFsU\nYs852V1Eu6oAU85qeI4u+XliIrM1gof/T2JPNz0V5QasLktJ1bpEnayOk3IZVuYQ\nQ+vr21Ta78qqxDZ947/QL+/a0iX/CrOyxCP8Bq6G3SkawvKkgHVxREo/gOXQI5pT\nsWCyIcylkxqVdMADG33iPFDABLtIoIsGoOtyo5jpAoGAZAYoHboe6jxU+KY03NKU\n4fkYMscNOogqdD/gCeny+IEXRwcI1R6espUQdeBWo2Z7qQ6uikGMMLkrXm/po2lf\nZgDVD9i+tY8MbFWpdQMZ0CSOVC+CES5ByB1xfV7Et2VE2VU62MbJz+7nRNsQrTll\nnpPloEPbeMz2n0c0NTdf1uU=\n-----END PRIVATE KEY-----\n",
+    client_email: "africanstore@contract-ai-453807.iam.gserviceaccount.com",
+    client_id: "109619164587319555589",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/africanstore%40contract-ai-453807.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com",
+  }, // Absolute path relative to the current file
+  //keyFile: path.join(__dirname, "google.json"), // Absolute path relative to the current file
   scopes: [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive", // Needed for sharing
