ab1b870791246f3795b8995704a9e1c7fa679c62
Ukagha Promise <85031254+backendbro@users.noreply.github.com>
2025-03-31 13:36:05 +0600

    Update index.js

--- src/email-views/index.js
+++ src/email-views/index.js
@@ -6,7 +6,12 @@ const welcomeSource = fs.readFileSync(
   path.resolve(__dirname, "./welcome.handlebars"),
   "utf8"
 );
+const workerSource = fs.readFileSync(
+  path.resolve(__dirname, "./worker.handlebars"),
+  "utf8"
+);
 
 const welcomeEmailTemplate = handlebars.compile(welcomeSource);
+const workerEmailTemplate = handlebars.compile(workerSource);
 
-module.exports = { welcomeEmailTemplate };
+module.exports = { welcomeEmailTemplate, workerEmailTemplate };
