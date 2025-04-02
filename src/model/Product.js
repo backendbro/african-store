37af804deb5912512e71e815cf2a6beaaf81dc9b
Ukagha Promise <85031254+backendbro@users.noreply.github.com>
2025-04-02 13:41:54 +0600

    Update Product.js

--- src/model/Product.js
+++ src/model/Product.js
@@ -39,7 +39,7 @@ const ProductSchema = new mongoose.Schema(
         return val;
       },
     },
-    DiscountiscountType: {
+    DiscountType: {
       type: String,
       enum: ["No Discount", "Percentage", "Fixed Amount"],
       default: "No Discount",
